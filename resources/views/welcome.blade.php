ff33f09bfde71aceb83c0f4ec4df9d2ee008d2b0
jraihan-me <jahirraihanmsd@gmail.com>
2022-12-21 16:45:26 +0600

    update with tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,8 @@
 <h1>This is from docker container</h1>
 <h3>This is updated</h3>
+@php
+ $users = \App\Models\User::all();   
+@endphp
+@foreach ($users as $user)
+    <p>{{ $user->name }}</p>
+@endforeach
